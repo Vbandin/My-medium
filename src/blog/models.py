18de6d4da895b361f8993c42f03fce199d18a7bb
Victor Bandin <victorbandin@gmail.com>
2017-03-12 00:05:12 +0100

    creo modelo blog, superadmin y hago migraciones

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    PUBLISHED = "PUB"
+    DRAFT = "DFT"
+    STATUSES = (
+        (PUBLISHED, "Published"),
+        (DRAFT, "Draft")
+    )
+
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=250, blank=True)
+    content = models.TextField(blank=True)
+    status = models.CharField(max_length=3, default=DRAFT, choices=STATUSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
